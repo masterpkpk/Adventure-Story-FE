d642fd852b70d844765625294b1596eabbb27496
william <masterpkpk@gmail.com>
2021-03-08 12:29:32 -0500

    added some functions

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,3 +1,24 @@
-function yoyo() {
+const user = ""
+
+function main() {
+  return document.getElementById("main")
+}
+
+function resetMain() {
+  main().innerHTML = ""
+}
+
+function getUserName(input) {
+  user = input
+}
+
+function nameTemplate() {
   
 }
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  getUserName()
+})
