36778189dc6f3c4958e2180e9e90a97d0756f585
william <masterpkpk@gmail.com>
2021-03-08 12:38:07 -0500

    added more functinons

--- javascripts/index.js
+++ javascripts/index.js
@@ -8,14 +8,31 @@ function resetMain() {
   main().innerHTML = ""
 }
 
+function form() {
+  return document.getElementById("form")
+}
+
 function getUserName(input) {
   user = input
 }
 
 function nameTemplate() {
-
+  return `
+    <h3>Enter your Hero's Name</h3>
+    <form id="form">
+    <div class="input-field">
+      <label for="name">Name</label>
+      <input type="text" name="name" id="name">
+    </div>
+    `
 }
-asd
+
+function renderNameTemplate() {
+  resetMain()
+  main().innerHTML = nameTemplate()
+  
+}
+
 
 
 
