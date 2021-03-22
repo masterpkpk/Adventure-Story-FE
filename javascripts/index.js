654f9ba770d11a6d28e412b9612d58e270ed9ff8
william <masterpkpk@gmail.com>
2021-03-22 12:17:33 -0400

    made changes to story checkpoint flow

--- javascripts/index.js
+++ javascripts/index.js
@@ -2,13 +2,13 @@ const resetMain = () => main().innerHTML = ""
 const knight = () => User.avatarFetch(knightPic)
 const wolf = () => User.avatarFetch(wolfPic)
 const onion = () => User.avatarFetch(onionPic)
-const rollDisplay = () => (resetMain(), main().innerHTML = `${rollDie()}`)
+const rollDisplay = (checkpoint) => (resetMain(), main().innerHTML = `${rollDie(checkpoint)}`)
 const search = () => (Choice.createChoiceObj("search", 1), Story.updateCheckPoint(1), rollTemplate())
-const  door = () =>(Choice.createChoiceObj("door", 2), Story.updateCheckPoint(2), rollTemplate())
+const  door = () =>(Choice.createChoiceObj("door", 2), rollTemplate(2))
 
 
-function rollDie() {
-
+function rollDie(checkpoint) {
+  Story.updateCheckPoint(checkpoint)
   min = Math.ceil(1);
   max = Math.floor(7);
   return Math.floor(Math.random() * (7 - 1) + 1);
@@ -16,15 +16,15 @@ function rollDie() {
 }
 
 
-function rollTemplate() {
-
+function rollTemplate(checkpoint) {
+  
   resetMain() 
   main().innerHTML = `
   
   <div class="container">
     <h1> Roll to decide your fate </h1>
     <input type="hidden" id="roll" >
-    <p> <button onclick="return rollDisplay() " style="background-color: transparent" id="Ok">${diePic}</button> </p>
+    <p> <button onclick="return rollDisplay(${checkpoint}) " style="background-color: transparent" id="Ok">${diePic}</button> </p>
   </div>
   `
 }
